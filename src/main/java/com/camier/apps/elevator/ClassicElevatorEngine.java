0b4c148c8b80692dd2642519ab7c7992d1d3904a
Camier Mickael <camier.mickael@gmail.com>
2013-10-11 02:17:58 +0200

    Working in progress,half terminated hazardous algo

--- src/main/java/com/camier/apps/elevator/ClassicElevatorEngine.java
+++ src/main/java/com/camier/apps/elevator/ClassicElevatorEngine.java
@@ -3,7 +3,7 @@
  */
 package com.camier.apps.elevator;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 /**
  * @author fufuuu
@@ -12,25 +12,19 @@ import java.util.LinkedList;
 public class ClassicElevatorEngine 
 	implements IElevatorEngine {
 
-	private LinkedList<Call> callPool;
+	private ArrayList<Call> pendingCalls;
+	private ArrayList<Call> processingCalls;
 	private int currentFloor;
 	private boolean isDoorOpened;
 	private int[] floorsIntended;
 	private final int totalFloors;
 	
-	private int i;
+	private Direction lastDirection;
+	
 	
 	public ClassicElevatorEngine(final int _totalFloors) {
-		reset();
 		totalFloors = _totalFloors;
-	}
-	
-	public final LinkedList<Call> getCallPool() {
-		return callPool;
-	}
-	
-	private final void setCallPool(final LinkedList<Call> callPool) {
-		this.callPool = callPool;
+		reset();
 	}
 	
 	public final int getCurrentFloor() {
@@ -43,7 +37,26 @@ public class ClassicElevatorEngine
 	
 	@Override
 	public void call(Call call) {
-		this.getCallPool().addLast(call);
+		
+		if(lastDirection != null) {
+			if (lastDirection == Direction.UP) {
+				if(call.getFromFloor() >= currentFloor) {
+					processingCalls.add(call);
+				}
+				else {
+					pendingCalls.add(call);
+				}
+			} else {
+				if(call.getFromFloor() <= currentFloor) {
+					processingCalls.add(call);
+				}
+				else {
+					pendingCalls.add(call);
+				}
+			}
+		} else {
+			processingCalls.add(call);
+		}
 	}
 	
 	public boolean isDoorOpened() {
@@ -61,37 +74,47 @@ public class ClassicElevatorEngine
 
 	@Override
 	public void reset() {
-		setCallPool(new LinkedList<Call>());
-		setCurrentFloor(1);
+		pendingCalls = new ArrayList<Call>();
+		processingCalls = new ArrayList<Call>();
+		lastDirection = null;
+		setCurrentFloor(0);
 		closeDoor();
 		floorsIntended = new int[totalFloors];
 		for(int i = 0 ; i < totalFloors ; i++) {
 			floorsIntended[i] = 0;
 		}
-		i = 1;
 	}
 	
 	@Override
 	public Command getNextCommand() {
 		if(isDoorOpened()) {
-			closeDoor();
-			return Command.CLOSE;
-		} else {
-			if( callPool.size() > 0 ) {
-				if( callPool.getFirst().getFromFloor() > getCurrentFloor() ) {
-					setCurrentFloor(getCurrentFloor() + 1); 
-					return Command.UP;
-				} else if( callPool.getFirst().getFromFloor() < getCurrentFloor() ) {
-					setCurrentFloor(getCurrentFloor() - 1); 
-					return Command.DOWN;
-				} else {
-					callPool.pollFirst();
-					openDoor();
-					return Command.OPEN;
-				}
-			}
-			else {
-				return Command.NOTHING;
+			closeDoor(); return Command.CLOSE;
+		}			
+		
+		if(floorsIntended[currentFloor] > 0) {
+			floorsIntended[currentFloor] = 0;
+			openDoor(); return Command.OPEN;
+		}
+		
+		if(processingCalls.size() <= 0 && pendingCalls.size() > 0) {
+			// write logic to get new processingCalls from pendingCalls
+			processingCalls = pendingCalls;
+			pendingCalls = new ArrayList<Call>();
+		}
+		
+		if(processingCalls.size() <= 0) {
+			return Command.NOTHING;
+		}
+		else {
+			if(processingCalls.get(0).getFromFloor() > currentFloor) {
+				currentFloor++;
+				lastDirection = Direction.UP; return Command.UP;
+			} else if(processingCalls.get(0).getFromFloor() < currentFloor) {
+				currentFloor--;
+				lastDirection = Direction.DOWN; return Command.DOWN;
+			} else {
+				processingCalls.remove(processingCalls.size()-1);
+				openDoor(); return Command.OPEN;	
 			}
 		}
 	}
