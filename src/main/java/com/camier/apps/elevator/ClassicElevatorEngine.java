9c116f8ba4c01e40088600e1b5d170dec0f08435
Camier Mickael <camier.mickael@gmail.com>
2013-10-11 19:39:49 +0200

    Refactoring some elevatorEngine's method

--- src/main/java/com/camier/apps/elevator/ClassicElevatorEngine.java
+++ src/main/java/com/camier/apps/elevator/ClassicElevatorEngine.java
@@ -3,7 +3,8 @@
  */
 package com.camier.apps.elevator;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author fufuuu
@@ -12,8 +13,8 @@ import java.util.ArrayList;
 public class ClassicElevatorEngine 
 	implements IElevatorEngine {
 
-	private ArrayList<Call> pendingCalls;
-	private ArrayList<Call> processingCalls;
+	private Set<Call> pendingCalls;
+	private Set<Call> processingCalls;
 	private int currentFloor;
 	private boolean isDoorOpened;
 	private int[] floorsIntended;
@@ -35,47 +36,71 @@ public class ClassicElevatorEngine
 		this.currentFloor = currentPosition;
 	}
 	
-	@Override
-	public void call(Call call) {
-		
-		if(lastDirection != null) {
-			if (lastDirection == Direction.UP) {
-				if(call.getFromFloor() >= currentFloor) {
-					processingCalls.add(call);
-				}
-				else {
-					pendingCalls.add(call);
-				}
-			} else {
-				if(call.getFromFloor() <= currentFloor) {
-					processingCalls.add(call);
-				}
-				else {
-					pendingCalls.add(call);
-				}
-			}
-		} else {
-			processingCalls.add(call);
-		}
-	}
-	
 	public boolean isDoorOpened() {
 		return isDoorOpened;
 	}
 	
-	public void closeDoor() {
+	private Command closeDoor() {
 		this.isDoorOpened = false;
+		return Command.CLOSE;
 	}
 	
-	public void openDoor() {
+	private Command openDoor() {
 		this.isDoorOpened = true;
+		return Command.OPEN;
+	}
+	
+	private Command goUp() {
+		if (isDoorOpened()) {
+			return closeDoor();
+		}
+		
+		if(currentFloor < totalFloors) { 
+			this.currentFloor++;
+			lastDirection = Direction.UP;
+			return Command.UP;
+		}
+		return Command.NOTHING;
+	}
+	
+	private Command goDown() {
+		if (isDoorOpened()) {
+			return closeDoor();
+		}
+		
+		if(currentFloor > 0) { 
+			this.currentFloor--;
+			lastDirection = Direction.DOWN;
+			return Command.DOWN;
+		}
+		return Command.NOTHING;
+	}
+	
+	private boolean isCallOnMyWay(Call call) {
+		if(lastDirection != null) {
+			if( currentFloor < call.getFromFloor() ) {
+				return (lastDirection == Direction.UP) ? true : false;
+			} else {
+				return (lastDirection == Direction.DOWN) ? true : false;
+			}
+		} else {
+			return true; // TODO Check if return true when lastDirection = null is a good practice
+		}
 	}
 
+	@Override
+	public void call(Call call) {
+		if(isCallOnMyWay(call)) {
+			processingCalls.add(call);
+		} else {
+			pendingCalls.add(call);
+		}
+	}
 
 	@Override
 	public void reset() {
-		pendingCalls = new ArrayList<Call>();
-		processingCalls = new ArrayList<Call>();
+		pendingCalls = new HashSet<Call>();
+		processingCalls = new HashSet<Call>();
 		lastDirection = null;
 		setCurrentFloor(0);
 		closeDoor();
@@ -87,36 +112,35 @@ public class ClassicElevatorEngine
 	
 	@Override
 	public Command getNextCommand() {
-		if(isDoorOpened()) {
-			closeDoor(); return Command.CLOSE;
-		}			
-		
-		if(floorsIntended[currentFloor] > 0) {
-			floorsIntended[currentFloor] = 0;
-			openDoor(); return Command.OPEN;
-		}
-		
-		if(processingCalls.size() <= 0 && pendingCalls.size() > 0) {
-			// write logic to get new processingCalls from pendingCalls
-			processingCalls = pendingCalls;
-			pendingCalls = new ArrayList<Call>();
-		}
-		
-		if(processingCalls.size() <= 0) {
-			return Command.NOTHING;
-		}
-		else {
-			if(processingCalls.get(0).getFromFloor() > currentFloor) {
-				currentFloor++;
-				lastDirection = Direction.UP; return Command.UP;
-			} else if(processingCalls.get(0).getFromFloor() < currentFloor) {
-				currentFloor--;
-				lastDirection = Direction.DOWN; return Command.DOWN;
-			} else {
-				processingCalls.remove(processingCalls.size()-1);
-				openDoor(); return Command.OPEN;	
-			}
-		}
+//		if(isDoorOpened()) {
+//			return closeDoor(); 
+//		}			
+//		
+//		if(floorsIntended[currentFloor] > 0) {
+//			floorsIntended[currentFloor] = 0;
+//			return openDoor();
+//		}
+//		
+//		if(processingCalls.size() <= 0 && pendingCalls.size() > 0) {
+//			processingCalls = pendingCalls;
+//			pendingCalls = new HashSet<Call>();
+//		}
+//		
+//		if(processingCalls.size() <= 0) {
+//			return Command.NOTHING;
+//		}
+//		else {
+//			
+//			if(processingCalls.get(0).getFromFloor() > currentFloor) {
+//				return goUp();
+//			} else if(processingCalls.get(0).getFromFloor() < currentFloor) {
+//				return goDown();
+//			} else {
+//				processingCalls.remove(processingCalls.size()-1);
+//				return openDoor();	
+//			}
+//		}
+		return Command.NOTHING;
 	}
 
 	@Override
