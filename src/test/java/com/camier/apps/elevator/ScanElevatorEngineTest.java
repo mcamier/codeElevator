4743a86741f30e8ae87a5d80e0ce6a5c9a71a6f6
Camier Mickael <camier.mickael@gmail.com>
2013-10-12 00:52:48 +0200

    Add a few tests

--- src/test/java/com/camier/apps/elevator/ScanElevatorEngineTest.java
+++ src/test/java/com/camier/apps/elevator/ScanElevatorEngineTest.java
@@ -11,7 +11,7 @@ public class ScanElevatorEngineTest {
 	}
 	
 	@Test
-	public void elevatorMustGoUpAfterUniqueCallingTest() {
+	public void mustGoUpAfterUniqueCallingTest() {
 		ScanElevatorEngine elevator = new ScanElevatorEngine(6);
 		elevator.setCurrentFloor(0);
 		elevator.call(new Call(5, Direction.DOWN));
@@ -20,7 +20,7 @@ public class ScanElevatorEngineTest {
 	}
 	
 	@Test
-	public void elevatorMustDownUpUniqueCallingTest() {
+	public void mustDownUpUniqueCallingTest() {
 		ScanElevatorEngine elevator = new ScanElevatorEngine(6);
 		elevator.setCurrentFloor(5);
 		elevator.call(new Call(1, Direction.UP));
@@ -29,11 +29,38 @@ public class ScanElevatorEngineTest {
 	}
 	
 	@Test
-	public void elevatorEngineReturnsOpenWhenOpen() {
+	public void returnsCommandOpenWhenOpenOrderTest() {
 		ScanElevatorEngine elevator = new ScanElevatorEngine(6);
 		
 		Command result = elevator.openDoor();
 		Assert.assertEquals(Command.OPEN, result);
 		Assert.assertEquals(elevator.isDoorOpened(), true);
+	}
+	
+	@Test
+	public void returnsCommandCloseWhenCloseOrderTest() {
+		ScanElevatorEngine elevator = new ScanElevatorEngine(6);
+		
+		Command result = elevator.closeDoor();
+		Assert.assertEquals(Command.CLOSE, result);
+		Assert.assertEquals(elevator.isDoorOpened(), false);
+	}
+	
+	@Test
+	public void haveToCloseDoorBeforeGoUpTest() {
+		ScanElevatorEngine elevator = new ScanElevatorEngine(6);
+		elevator.openDoor();
+		
+		Command result = elevator.goUp();
+		Assert.assertEquals(Command.CLOSE, result);
+	}
+	
+	@Test
+	public void haveToCloseDoorBeforeGoDownTest() {
+		ScanElevatorEngine elevator = new ScanElevatorEngine(6);
+		elevator.openDoor();
+		
+		Command result = elevator.goDown();
+		Assert.assertEquals(Command.CLOSE, result);
 	}
 }
